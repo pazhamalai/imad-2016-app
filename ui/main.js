79a72098fcdc0e183752e7eab09005fec258b23b
pazhamalai <pazh14227.ee@rmkec.ac.in>
2016-10-04 14:29:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var marginLeft=0;
 function moveRight()
 {
   marginLeft=marginLeft+5;
-  element.style.marginLeft=marginleft+'px';
+  element.style.marginLeft=marginleft + 'px';
 }
 element.onclick = function()
 {
