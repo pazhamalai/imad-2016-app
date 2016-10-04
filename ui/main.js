21219661c04af57a401761c12324d3c1dbca7c8b
pazhamalai <pazh14227.ee@rmkec.ac.in>
2016-10-04 15:19:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,8 @@
 console.log('Loaded!');
 var img= document.getElementById('logo');
+var element=document.getElementById('madi');
 var marginLeft=0;
+var movel=0;
 function moveRight()
 {
   marginLeft=marginLeft + 5;
@@ -9,4 +11,13 @@ function moveRight()
 img.onclick = function ()
 {
     var interval= setInterval(moveRight , 100);
+};
+function moveR()
+{
+  movel=movel + 5;
+  element.style.marginLeft=movel + 'px';
+}
+element.onclick=function()
+{
+  var inter=setInterval(moveR,100);  
 };
