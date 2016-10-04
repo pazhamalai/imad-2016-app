72ec1fb2de99d7595b06eaaf08fa44508d68ae2f
pazhamalai <pazh14227.ee@rmkec.ac.in>
2016-10-04 14:35:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ function moveRight()
   marginLeft=marginLeft+5;
   element.style.marginLeft=marginleft + 'px';
 }
-element.onclick = function()
+element.onclick = function ()
 {
     var interval=setInterval(moveRight,100);
 };
