eca3d3190db138f44ece53467b6d81ef6e94e493
pazhamalai <pazh14227.ee@rmkec.ac.in>
2016-10-04 14:19:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element= document.getElementById('logo');
-img.onclick = function()
+element.onclick = function()
 {
-    img.style.marginLeft='100px';
+    element.style.marginLeft='100px';
 };
