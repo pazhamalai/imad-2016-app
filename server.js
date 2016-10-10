2530d985964dc9f65c6559b706b843540bb5256e
pazhamalai <pazh14227.ee@rmkec.ac.in>
2016-10-10 19:44:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/ui/logo.png', function (req, res) {
 var counter= 0;
 app.get('/counter', function(req, res){
    counter=counter +1; 
-   res.send(counter,toString());
+   res.send(counter,String());
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
