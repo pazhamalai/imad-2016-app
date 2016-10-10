efd2ee6d6cdaeb797eac3f317d547315d2f1f78e
pazhamalai <pazh14227.ee@rmkec.ac.in>
2016-10-10 20:39:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,6 +21,6 @@ button.onclick = function()
   
 };
 
-request.open('GET','http://pazhamalai.imad.hasura-app.io/counter',true);
-request.send(null);
+httprequest.open('GET','http://pazhamalai.imad.hasura-app.io/counter',true);
+httprequest.send(null);
 
