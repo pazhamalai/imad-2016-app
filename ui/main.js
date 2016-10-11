2bdcc72c0ce1f93d04e08ee1a7938a2eadabcdb9
pazhamalai <pazh14227.ee@rmkec.ac.in>
2016-10-11 20:22:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,17 @@ request.open('GET','http://pazhamalai.imad.hasura-app.io/counter',true);
 request.send(null);
 
 };
+
+var nameinput=document.getElementById('name');
+var name=nameinput.value;
+var submit=document.getElementById('submitbtn');
+submit.onclick=function()
+{
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0; i<names.length ;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
