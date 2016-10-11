896e4398e4454edc50e62bc479a0487bfde2b963
pazhamalai <pazh14227.ee@rmkec.ac.in>
2016-10-11 19:48:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.get('/ui/logo.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
 });
 
-var counter= 0;
 app.get('/counter', function(req, res){
    counter=counter + 1; 
    res.send(counter.toString());
