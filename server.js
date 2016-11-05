997adfab806b551f59df1214fc361b4ad63720fd
M.PAZHAMALAI <pazh14227.ee@rmkec.ac.in>
2016-11-05 22:04:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,10 +92,10 @@ var articles={
 
 </div>
 <div>
-       <p>October 30</p>
+       <p class='two'>October 30</p>
 </div>
 <div>
-       <p>This is my first article</p>
+       <p class='two'>This is my first article</p>
 </div>
 <div>
        <a href='https://www.google.co.in/imgres?imgurl=https%3A%2F%2Fpixabay.com%2Fstatic%2Fuploads%2Fphoto%2F2015%2F04%2F04%2F19%2F13%2Fone-706897_640.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fen%2Fone-1-number-design-collection-706897%2F&docid=Dxk_1n6Yd4M41M&tbnid=H9VLy5g15akogM%3A&w=640&h=640&hl=en&bih=659&biw=1366&ved=0ahUKEwiF5ruwy4LQAhXIK48KHYslB3AQMwg-KAwwDA&iact=mrc&uact=8' target='_blank'><img src='/ui/one.png' alt='article one' class='img-medium'/></a>
@@ -131,10 +131,10 @@ var articles={
 <a href='/articletwo' class='buttonone'>Article two</a>
 </div>
 <div>
-<p>October 30</p>
+<p class='two'>October 30</p>
 </div>
 <div>
-<p>This is my third article</p>
+<p class='two'>This is my third article</p>
 </div>
 <div>
 <a href='https://www.google.co.in/imgres?imgurl=http%3A%2F%2Fwww.freefoto.com%2Fimages%2F2000%2F03%2F2000_03_52---Number-Three_web.jpg&imgrefurl=http%3A%2F%2Fwww.freefoto.com%2Fpreview%2F2000-03-52%2FNumber-Three&docid=snq6DZOc3ApG4M&tbnid=fDyLAxJU8PMUmM%3A&w=400&h=600&hl=en&bih=659&biw=1366&ved=0ahUKEwiZxpCZzYLQAhWKt48KHazwDa44ZBAzCDgoNjA2&iact=mrc&uact=8' target='_blank'><img src='/ui/three.jpeg' alt='third article' class='img-medium'/></a>
