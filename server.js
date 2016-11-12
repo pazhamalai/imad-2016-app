0f845e0f728489933bd100bde4e61616d7ef0f37
M.PAZHAMALAI <pazh14227.ee@rmkec.ac.in>
2016-11-13 01:41:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -183,9 +183,25 @@ var htmltemplate=`<!DOCTYPE html>
 return htmltemplate;
 }
 
-app.get('/:articlename',function(req,res){
-    var articlename=req.params.articlename;
-     res.send(createtemplate(articles[articlename]));
+app.get('/articles/:articlename',function(req,res){
+    pool.query('SELECT *FROM articlea WHERE heading=&1',[req.params.articlename],function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           if(result.rows.length === 0){
+               res.status(404).send('Article not found');
+           }
+           else
+           {
+               var articleData=result.rows[0];
+           res.send(createtemplate(articleData));
+           }
+       }
+        
+    });
+     
 });
 
 
